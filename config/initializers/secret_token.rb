b23e9d961183170a46762c93d5dfd79ee5e44fae
connor@appacademy.io <connor@appacademy.io>
2013-06-10 11:10:46 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BetaMiners::Application.config.secret_token = '80ac0f14e8ed637530036f9b66cedab9b9e8befa2c3c1214e92f58dbad8cabb34d3f54d5ae8c6b881cf027690e26da7ba1992cc5a2649e25a806082daceb90b4'
