ce4d9c016f9732f227ca3d304d86612da9a142ee
Natasha Badillo <natasha.badillo@gmail.com>
2013-06-13 11:39:45 -0400

    Add access buttons to project page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -24,4 +24,8 @@ module ApplicationHelper
     link_to_function(name, "addFields(this, \"#{association}\",
                      \"#{escape_javascript(fields)}\")")
   end
+
+  def url_with_protocol(url)
+    /^http/.match(url) ? url : "http://#{url}"
+  end
 end
