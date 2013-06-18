064a51897f31ffbda9300a5b6934b10b75fe5d2d
App Academy Student <student@appacademy.io>
2013-06-18 16:34:45 -0400

    Add profile to new non-facebook signup

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,7 +1,7 @@
 class Profile < ActiveRecord::Base
   attr_accessible :user_id, :birthday, :gender, :location
 
-  belongs_to :user
+  belongs_to :user, inverse_of: :profile
 
   validates :gender, inclusion: {in: ["male", "female"]}
 end
