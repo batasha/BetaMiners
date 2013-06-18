edded68b0a968e0f78b8216b4ef5cc25274b3fcd
App Academy Student <student@appacademy.io>
2013-06-18 10:59:47 -0400

    add fade-out/in to survey fields

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,12 +18,11 @@
 
 function removeFields(link) {
   $(link).prev("input[type=hidden]").val("1");
-  $(link).closest(".fields").fadeOut(300);
+  $(link).closest(".fields").fadeOut();
 }
 
 function addFields(link, association, content) {
   var newId = new Date().getTime();
   var regEx = new RegExp("new_" + association, 'g');
-  $(link).parent().before(content.replace(regEx, newId)).parent(":last-child").hide().fadeIn();
-  $(link + ":last-child").hide().fadeIn();
+  $(link).parent().before(content.replace(regEx, newId)).prev().hide().fadeIn();
 }
