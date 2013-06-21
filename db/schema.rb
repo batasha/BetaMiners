d124a2c77c9ece63d8233ede47fc81969a9b7982
Natasha Badillo <natasha.badillo@gmail.com>
2013-06-21 13:38:20 -0400

    add missing timestamps to tables

--- db/schema.rb
+++ db/schema.rb
@@ -11,39 +11,39 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130613190030) do
+ActiveRecord::Schema.define(:version => 20130621173037) do
 
   create_table "choices", :force => true do |t|
-    t.integer "feedback_id"
-    t.integer "response_id"
-    t.text    "comment"
+    t.integer  "feedback_id"
+    t.integer  "response_id"
+    t.text     "comment"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "choices", ["feedback_id"], :name => "index_choices_on_feedback_id"
   add_index "choices", ["response_id"], :name => "index_choices_on_response_id"
 
   create_table "feedbacks", :force => true do |t|
-    t.integer "survey_id"
-    t.integer "user_id"
-    t.string  "os"
-    t.string  "browser"
-    t.string  "version"
+    t.integer  "survey_id"
+    t.integer  "user_id"
+    t.string   "os"
+    t.string   "browser"
+    t.string   "version"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "feedbacks", ["survey_id"], :name => "index_feedbacks_on_survey_id"
   add_index "feedbacks", ["user_id"], :name => "index_feedbacks_on_user_id"
 
-  create_table "platforms", :force => true do |t|
-    t.string "os"
-    t.string "browser"
-    t.string "version"
-  end
-
   create_table "profiles", :force => true do |t|
-    t.date    "birthday"
-    t.string  "gender"
-    t.string  "location"
-    t.integer "user_id"
+    t.date     "birthday"
+    t.string   "gender"
+    t.string   "location"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "profiles", ["user_id"], :name => "index_profiles_on_user_id"
@@ -59,28 +59,36 @@ ActiveRecord::Schema.define(:version => 20130613190030) do
     t.string   "logo_content_type"
     t.integer  "logo_file_size"
     t.datetime "logo_updated_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "projects", ["user_id"], :name => "index_projects_on_user_id"
 
   create_table "questions", :force => true do |t|
-    t.integer "survey_id"
-    t.string  "text"
+    t.integer  "survey_id"
+    t.string   "text"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "questions", ["survey_id"], :name => "index_questions_on_survey_id"
 
   create_table "registrations", :force => true do |t|
-    t.integer "user_id"
-    t.integer "project_id"
+    t.integer  "user_id"
+    t.integer  "project_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "registrations", ["project_id"], :name => "index_registrations_on_project_id"
   add_index "registrations", ["user_id"], :name => "index_registrations_on_user_id"
 
   create_table "responses", :force => true do |t|
-    t.integer "question_id"
-    t.string  "text"
+    t.integer  "question_id"
+    t.string   "text"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "responses", ["question_id"], :name => "index_responses_on_question_id"
@@ -92,21 +100,27 @@ ActiveRecord::Schema.define(:version => 20130613190030) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "screenshots", ["project_id"], :name => "index_screenshots_on_project_id"
 
   create_table "surveys", :force => true do |t|
-    t.integer "test_phase_id"
+    t.integer  "test_phase_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "surveys", ["test_phase_id"], :name => "index_surveys_on_test_phase_id"
 
   create_table "test_phases", :force => true do |t|
-    t.string  "name"
-    t.date    "start_date"
-    t.date    "end_date"
-    t.integer "project_id"
+    t.string   "name"
+    t.date     "start_date"
+    t.date     "end_date"
+    t.integer  "project_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "test_phases", ["project_id"], :name => "index_test_phases_on_project_id"
